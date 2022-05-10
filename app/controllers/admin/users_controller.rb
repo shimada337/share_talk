3e521298d741e447bedca31875c08272e82a95c1
shimada337 <eisukeqoo@icloud.com>
2022-05-10 13:36:29 +0000

    [add]admin会員詳細

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,26 @@
 class Admin::UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(10)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash[:notice] = "ユーザー情報を更新しました"
+    redirect_to admin_user_path(@user)
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :self_introduction, :is_deleted)
   end
 end
