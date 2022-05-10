6447b127f92b9e2c481a3f2e9b376213cea30777
shimada337 <eisukeqoo@icloud.com>
2022-05-10 11:47:17 +0000

    [fix]投稿削除、投稿編集

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -12,12 +12,25 @@ class User::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
   end
   
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
+    redirect_to posts_path
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
     redirect_to posts_path
   end
   
