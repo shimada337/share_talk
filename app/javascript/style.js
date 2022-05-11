258e88398bca30f12896bdd3bf8febee55069cff
shimada337 <eisukeqoo@icloud.com>
2022-05-11 11:00:40 +0000

    [update]タブメニュー

--- app/javascript/style.js
+++ app/javascript/style.js
@@ -1,1 +1,10 @@
 console.log("test")
+$('#tab-contents .tab[id != "post_index"]').hide();
+
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
+});
