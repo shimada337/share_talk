e06c4a9041571619d0d3d9fd19eb056005790593
shimada337 <eisukeqoo@icloud.com>
2022-05-14 07:06:15 +0000

    [add]論理削除

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User::SessionsController < Devise::SessionsController
+  before_action :user_state, only: [:create]
+
   # before_action :configure_sign_in_params, only: [:create]
   
   def after_sign_in_path_for(resource)
@@ -26,7 +28,16 @@ class User::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  
+  # 退会しているかを判断するメソッド
+def user_state
+  @user = User.find_by(email: params[:user][:email])
+  return if !@user
+  if @user.valid_password?(params[:user][:password]) && @user.is_deleted
+    redirect_to new_user_registration_path
+  end
+end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
