1c31384d03e5e64bcea245b54d16fe06ad13678d
shimada337 <eisukeqoo@icloud.com>
2022-05-15 06:41:06 +0000

    [add]検索機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
+  
+  def set_search
+    @search = User.ransack(params[:q])
+    @search_users = @search.result.page(params[:page])
+  end
 end
