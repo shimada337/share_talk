b03c9bd9aa89775a2637b99100939a83a9a9646a
shimada337 <eisukeqoo@icloud.com>
2022-05-16 08:54:57 +0000

    [fix]ゲストユーザー

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,7 +56,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(name: "guestuser", email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.name = "gue"
+      user.name = "guestuser"
     end
   end
   
