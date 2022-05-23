47641a75429ea50edfa6a7d78ad82f20d4125287
shimada337 <eisukeqoo@icloud.com>
2022-05-23 12:00:24 +0000

    [update]投稿画像ファイル表示

--- app/javascript/style.js
+++ app/javascript/style.js
@@ -12,7 +12,3 @@ document.addEventListener("turbolinks:load", function() {
     });
   })
 })
-
-$('.custom-file-input').on('change',function(){
-  $(this).next('.custom-file-label').html($(this)[0].files[0].name);
-})
