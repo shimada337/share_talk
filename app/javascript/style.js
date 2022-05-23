f5defff153ddfb537d0c1195cc1050d023a4e677
shimada337 <eisukeqoo@icloud.com>
2022-05-23 08:12:06 +0000

    [update]フォーム改行

--- app/javascript/style.js
+++ app/javascript/style.js
@@ -12,3 +12,7 @@ document.addEventListener("turbolinks:load", function() {
     });
   })
 })
+
+$('.custom-file-input').on('change',function(){
+  $(this).next('.custom-file-label').html($(this)[0].files[0].name);
+})
