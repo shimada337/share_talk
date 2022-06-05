e6dd8fa9b263e1bed9fc4e84f362180c4b269ef9
shimada337 <eisukeqoo@icloud.com>
2022-06-05 13:04:38 +0000

    [update]フォロー、フォロワー

--- app/controllers/user/relationships_controller.rb
+++ app/controllers/user/relationships_controller.rb
@@ -18,11 +18,11 @@ class User::RelationshipsController < ApplicationController
   # フォロー一覧
   def followings
     @user = User.find(params[:user_id])
-    @users = @user.followings
+    @users = @user.followings.page(params[:page]).per(20).order(created_at: :desc)
   end
   # フォロワー一覧
   def followers
     @user = User.find(params[:user_id])
-    @users = @user.followers
+    @users = @user.followers.page(params[:page]).per(20).order(created_at: :desc)
   end
 end
