1d8562fb4b61b994f972122923a7c46e23d63df8
shimada337 <eisukeqoo@icloud.com>
2022-06-09 04:05:16 +0000

    [update]フォロー非同期通信

--- app/controllers/user/relationships_controller.rb
+++ app/controllers/user/relationships_controller.rb
@@ -1,18 +1,25 @@
 class User::RelationshipsController < ApplicationController
   before_action :authenticate_user!
+  before_action :search
+  
+  def search
+    @search_user = User.ransack(params[:q])
+  end
   # フォローするとき
   def create
-     user = User.find(params[:user_id])
+    @search_users = @search_user.result.page(params[:page]).per(20).order(created_at: :desc)
+     @user = User.find(params[:user_id])
      if current_user.follow(params[:user_id])
-       user.create_notification_follow!(current_user)
-       redirect_to request.referer, notice: "#{user.name}をフォローしました"
+       @user.create_notification_follow!(current_user)
+      flash.now[:notice] = "#{@user.name}をフォローしました"
      end
   end
   # フォロー外すとき
   def destroy
-    user = User.find(params[:user_id])
+    @search_users = @search_user.result.page(params[:page]).per(20).order(created_at: :desc)
+    @user = User.find(params[:user_id])
     if current_user.unfollow(params[:user_id])
-      redirect_to request.referer, alert: "#{user.name}のフォローを外しました"
+      flash.now[:alert] = "#{@user.name}のフォローを外しました"
     end
   end
   # フォロー一覧
